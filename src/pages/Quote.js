2cbd53f835ae3cb7eb6a57f269bbe2454773c0b8
subhashchandrab <subhashchandra.b@gmail.com>
2022-02-22 11:46:51 +0530

    Update Quote.js

--- src/pages/Quote.js
+++ src/pages/Quote.js
@@ -1,11 +1,15 @@
 import GetQuoteForm from '../components/quote/GetQuoteForm';
+import QuoteInputForm from '../components/quote/QuoteInputForm';
 
 function QuotePage() {
     return (
-        <section>
-          <h1>Get Quote</h1>
-          <GetQuoteForm />
-        </section>
+      <div className="container">
+        <h3 className="entry-title">Generate quote for insurance</h3>
+        <p>
+            Quickly get the quotation for the insurance by choosing the product of your interest and  providing very minimal information.
+        </p>
+          <QuoteInputForm />
+        </div>
       );
 }
 
